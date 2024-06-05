73f88d253c00765ead78f445e10c38a3c08e7156
DemonForce <64558271+DemonForce@users.noreply.github.com>
2024-06-05 13:11:51 -0700

    Update index.js
    Cambios musica

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var audio = document.getElementById("backgroundAudio");
+var volumeControl = document.getElementById("volumeControl");
+var isPlaying = false;
 
+volumeControl.addEventListener("input", function () {
+  if (volumeControl.value > 0 && !isPlaying) {
+    audio.play();
+    isPlaying = true;
+  }
+  audio.volume = volumeControl.value;
+});
