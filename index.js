aef54a1e4f8b29c249a11238c5b3ed328e19f7fe
DemonForce <64558271+DemonForce@users.noreply.github.com>
2024-06-06 15:53:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ volumeControl.addEventListener("input", function () {
   }
   audio.volume = volumeControl.value;
 });
+
+// Función para generar un número aleatorio
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Generar un número aleatorio como versión
+var randomVersion = getRandomInt(1000000);
+
+// Añadir el parámetro aleatorio a los archivos CSS y JS
+document.write(
+  '<link rel="stylesheet" href="styles.css?v=' + randomVersion + '">'
+);
+document.write(
+  '<script src="index.js?v=' + randomVersion + '"><\/script>'
+);
